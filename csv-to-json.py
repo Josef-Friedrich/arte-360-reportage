31b60ba8addb3ededd2b1db2cd7e92bf074f3f94
Josef Friedrich <josef@friedrich.rocks>
2023-05-02 23:17:11 +0200

    episodes.json

--- csv-to-json.py
+++ csv-to-json.py
@@ -5,27 +5,42 @@ import json
 from typing import Any
 import re
 
-def extract_season_episode(season_episode: str)-> str:
-    match = re.match(r"S([0-9])+E[0-9]+", season_episode)
+
+def extract_season_episode(season_episode: str, episode: bool = False) -> str:
+    match = re.match(r"S([0-9]+)E([0-9]+)", season_episode)
     if match:
+        if episode:
+            return match[2]
         return match[1]
 
     raise Exception(season_episode)
 
-with open('episodes.csv', newline='') as csvfile:
 
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+def extract_duration(duration: str) -> int | None:
+    if duration != "":
+        return int(duration)
+
+
+with open("episodes.csv", newline="") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",", quotechar='"')
 
     episodes: list[dict[str, Any]] = []
     for episode in reader:
-        if episode[0] == 'season/episode':
+        if episode[0] == "season/episode":
             continue
 
-        j: dict[str, str] = {}
-        j['season'] = extract_season_episode(episode[0]),
-        j['episode'] = str(episode[0]),
-        j['no'] = episode[1]
-        j['title'] = episode[2]
-        episodes.append(j)
+        e: dict[str, str | int | None] = {}
+        e["season"] = int(extract_season_episode(episode[0]))
+        e["episode"] = int(extract_season_episode(episode[0], True))
+        e["season_episode_id"] = episode[0]
+        e["no"] = int(episode[1])
+        e["title"] = episode[2]
+        e["air_date"] = episode[3]
+        e["duration"] = extract_duration(episode[4])
+        e["thetvdb_episode_no"] = int(episode[6])
+        episodes.append(e)
 
-print(json.dumps(episodes, indent=2))
+json_dump: str = json.dumps(episodes, indent=2, ensure_ascii=False)
+
+with open("episodes.json", "w") as json_file:
+    json_file.write(json_dump)
