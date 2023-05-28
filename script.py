3d78355481db6c724afd11a9552df761ed6c7169
Josef Friedrich <josef@friedrich.rocks>
2023-05-28 17:28:38 +0200

    Unifed scripts

--- script.py
+++ script.py
@@ -1,11 +1,81 @@
+#! /usr/bin/env python
+
 from __future__ import annotations
 
 import typing
 from datetime import date
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from _tvshow import TvShowData
+
+import difflib
+import json
+
+
+import termcolor
+import yaml
+
+
+import argparse
+
+
+import re
+
+### lib #######################################################################
+
+
+def read_text_file(file_path: str) -> str:
+    with open(file_path, "r") as f:
+        return f.read()
+
+
+def clean_title(title: str) -> str:
+    title = title.replace("–", "-")
+    title = re.sub(r" *\(.+\) *", " ", title)
+    title = title.strip()
+    title = re.sub(r"  +", " ", title)
+    title = re.sub(r"GEO Reportage *[:-] +", "", title)
+    title = re.sub(r"^\d+ *- *", "", title)
+    return title
+
+
+def normalize_title(title: str) -> str:
+    title = title.replace(", ", " ")
+    title = title.replace(": ", " ")
+    title = title.replace(" - ", " ")
+    return title.lower()
+
+
+### dvd #######################################################################
+
+
+class DvdData(typing.TypedDict):
+    title: str
+    """for example ``KOLUMBIEN - Die rasenden Engel der Linea 5``"""
+
+    dvd_count: int
+    """for example ``1``"""
+
+    ean: str
+    """for example ``4009496401519``"""
+
+    release_date: str
+    """for example ``2007-01-01``"""
+
+    production_date: str
+    """for example ``2007-01-01``"""
+
+    duration: int
+    """for example ``52``"""
+
+    mediamops: str
+    """for example ``M0B09SDJQGBH``"""
+
+    asin: str
+    """for example ```B09SDJQGBH`"""
+
+    episodes: list[str]
+
+
+### episode ###################################################################
 
 
 class EpisodeData(typing.TypedDict):
@@ -257,3 +327,165 @@ class Episode:
     @property
     def year(self) -> int:
         return int(self.data["air_date"][0:4])
+
+
+### season ####################################################################
+
+
+class SeasonData(typing.TypedDict):
+    no: int
+    year: int
+    episodes: list[EpisodeData]
+
+
+class Season:
+    data: SeasonData
+
+    episodes: list[Episode]
+
+    def __init__(self, data: SeasonData, episodes: list[Episode]) -> None:
+        self.data: SeasonData = data
+        self.episodes = episodes
+
+    @property
+    def no(self) -> int:
+        return self.data["no"]
+
+    @property
+    def year(self) -> int:
+        return self.data["year"]
+
+
+### main ######################################################################
+
+
+class TvShowData(typing.TypedDict):
+    seasons: list[SeasonData]
+    databases: dict[str, str]
+
+
+YAML_FILENAME = "360-grad-reportage.yml"
+
+
+class TvShow:
+    data: TvShowData
+
+    titles: dict[str, int]
+
+    episodes: list[Episode]
+
+    seasons: list[Season]
+
+    def __init__(self) -> None:
+        self.data = self.__load()
+        self.__generate_season_episodes()
+        self.titles = self.__generate_title_list()
+
+    def __load(self) -> TvShowData:
+        with open(YAML_FILENAME, "r") as y:
+            result: TvShowData = yaml.load(y, Loader=yaml.Loader)
+            return result
+
+    def __generate_season_episodes(self) -> None:
+        self.episodes: list[Episode] = []
+        self.seasons: list[Season] = []
+        overall_no: int = 1
+        for season_data in self.data["seasons"]:
+            episodes: list[Episode] = []
+            episode_no: int = 1
+            for episode_data in season_data["episodes"]:
+                episode = Episode(
+                    episode_data,
+                    tv_show=self.data,
+                    overall_no=overall_no,
+                    season_no=season_data["no"],
+                    episode_no=episode_no,
+                )
+                self.episodes.append(episode)
+                episodes.append(episode)
+                overall_no += 1
+                episode_no += 1
+            self.seasons.append(Season(season_data, episodes))
+
+    def __generate_title_list(self) -> dict[str, int]:
+        titles: dict[str, int] = {}
+        for episode in self.episodes:
+            index: int = episode.overall_no - 1
+            titles[episode.title] = index
+            if episode.alias:
+                titles[episode.alias] = index
+            if episode.title_fr:
+                titles[episode.title_fr] = index
+            if episode.title_en:
+                titles[episode.title_en] = index
+        return titles
+
+    def get_episode_by_title(
+        self, title: str | None, debug: bool = False
+    ) -> Episode | None:
+        if not title:
+            return
+        found: list[str] = difflib.get_close_matches(title, self.titles.keys(), n=1)
+        episode = None
+        if len(found) > 0:
+            episode = self.episodes[self.titles[found[0]]]
+
+        if debug:
+            if not episode:
+                print(f"No match found for: {termcolor.colored(title, color='red')}")
+            elif normalize_title(title) != normalize_title(episode.title):
+                print(
+                    f"{termcolor.colored(title, color='yellow')} <> {termcolor.colored(episode.title, color='blue')}"
+                )
+
+        return episode
+
+    # @property
+    # def episodes_data(self) -> list[EpisodeData]:
+    #     self.__add_indexes()
+    #     return self.data["episodes"]
+
+    # def reformat(self) -> None:
+    #     for old in self.episodes_data:
+    #         episode: dict[str, str | int] = {
+    #             "title": old["title"],
+    #             "air_date": old["air_date"],
+    #         }
+    #         if "duration" in old and old["duration"]:
+    #             episode["duration"] = old["duration"]
+
+    def export_to_json(self) -> None:
+        with open("360-grad-reportage.json", "w") as j:
+            json.dump(self.data, fp=j, indent=2, ensure_ascii=False)
+
+    def __write(self) -> None:
+        with open(YAML_FILENAME, "w") as y:
+            yaml.dump(self.data, stream=y, allow_unicode=True, sort_keys=False)
+
+    def save(self) -> None:
+        self.__write()
+
+
+tv_show = TvShow()
+
+
+### main ######################################################################
+
+
+def get_argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="arte-360-reportage-script")
+    parser.add_argument("-r", "--readme", action="store_true")
+    parser.add_argument("-j", "--json", action="store_true")
+
+    return parser
+
+
+def main() -> None:
+    args = get_argument_parser().parse_args()
+
+    if args.json:
+        tv_show.export_to_json()
+
+
+if __name__ == "__main__":
+    main()
