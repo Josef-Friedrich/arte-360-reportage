9c18995dad39ad70c2e731cb108e08e343075bdf
Josef Friedrich <josef@friedrich.rocks>
2023-06-01 14:42:35 +0200

    Reorder DVDs

--- arte_360_reportage.py
+++ arte_360_reportage.py
@@ -372,26 +372,26 @@ class DvdData(typing.TypedDict):
     title: str
     """for example ``KOLUMBIEN - Die rasenden Engel der Linea 5``"""
 
-    dvd_count: int
-    """for example ``1``"""
-
-    ean: str
-    """for example ``4009496401519``"""
-
     release_date: str
     """for example ``2007-01-01``"""
 
     production_date: str
     """for example ``2007-01-01``"""
 
-    duration: int
-    """for example ``52``"""
+    ean: str
+    """for example ``4009496401519``"""
 
     asin: str
     """for example ```B09SDJQGBH`"""
 
     mediamops: str
     """for example ``M0B09SDJQGBH``"""
+
+    dvd_count: int
+    """for example ``1``"""
+
+    duration: int
+    """for example ``52``"""
 
     episodes: list[str]
     """Refer to Episode titles"""
